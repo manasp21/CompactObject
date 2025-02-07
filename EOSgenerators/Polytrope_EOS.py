b7a98782aab81ccc709cd009dc2d0d056592214f
ChunHuangPhy <106232456+ChunHuangPhy@users.noreply.github.com>
2025-02-06 18:29:33 -0600

    Update Polytrope_EOS.py

--- EOSgenerators/Polytrope_EOS.py
+++ EOSgenerators/Polytrope_EOS.py
@@ -91,6 +91,32 @@ def fun_gamma_max(rho2, rho1, p1):
 
     gamma_max = root(fun, [3/4]).x[0]
     return gamma_max
+# ----------------------------------------------------------------------------
+# Copyright (c) 2020-2025 the NEoST core team
+#
+# This function is adopted from part of NEoST software.
+#
+# NEoST is free software: you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the Free
+# Software Foundation, either version 3 of the License, or (at your option) any
+# later version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+# PARTICULAR PURPOSE. See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along with this
+# program. If not, see <http://www.gnu.org/licenses/>.
+#
+# Additional permission under GNU GPL version 3 section 7:
+#
+# If you modify this Program, or any covered work, by linking or combining it with
+# MultiNest (or a modified version of that library), the licensors of this Program
+# grant you additional permission to convey the resulting work. Corresponding Source
+# for a non-source form of such a combination shall include the source code for the
+# parts of MultiNest used as well as that of the covered work.
+# ----------------------------------------------------------------------------
+#
 
 def eos_core_pp(gammas,rho_ts,rho_t,rho, P_t):
     P_ts = np.zeros(len(gammas))
